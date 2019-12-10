38ede7984aa0675856e0c2e58ea541d6121f5f68
Matthew Robertson <mattrobertson1414@gmail.com>
2019-12-10 11:17:54 -0700

    fixed some errors

--- morning-helper/morning-helper.ino
+++ morning-helper/morning-helper.ino
@@ -1,10 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include "./config.h"
 
 ESP8266WiFiMulti wifiMulti;
 WiFiClient wifiClient;
+HTTPClient http;
 PubSubClient client("172.20.10.14", 1883, wifiClient);
 String device_name = "esp8266-stop-light";
 const char* ssid1 = MATT_HOTSPOT_SSID;
@@ -12,7 +15,7 @@ const char* passwd1 = MATT_HOTSPOT_PASSWORD;
 
 int JACKET_LIGHT = D5;
 int BOOTS_LIGHT = D6;
-int UMBRELLA_LIGHT = D7:
+int UMBRELLA_LIGHT = D7;
 
 int jacket_threshold = 50;
 unsigned long timer_pointer = 0;
@@ -21,7 +24,7 @@ unsigned long five_minutes = 300000;
 int temp_current = 0;
 int temp_high = 0;
 int temp_low = 0;
-char* forcast = "";
+char* forecast = "";
 
 void connectToWifi(){
   WiFi.mode(WIFI_STA);
@@ -52,7 +55,6 @@ void reconnectToHub() {
       String message = "Stoplight (" + device_name + ") connected @ " + WiFi.localIP().toString();
       client.publish("/connections/stop-light", message.c_str());
       client.publish("homeassistant/sensor/jacket-threshold/config", "{\"name\": \"jacket-threshold\", \"state_topic\": \"/morning-helper/jacket-threshold\"}"); 
-      client.subscribe("/stop-light/change");
     } else {
       Serial.println("Connection to MQTT failed, trying again...");
       delay(3000);
@@ -61,10 +63,10 @@ void reconnectToHub() {
 }
 
 void updateWeather(){
-  String full_uri = "http://api.openweathermap.org/data/2.5/weather?zip=84604,us&APPID=65afbab12772f80f78a291fe057de213"
+  String full_uri = "http://api.openweathermap.org/data/2.5/weather?zip=84604,us&units=Imperial&APPID=65afbab12772f80f78a291fe057de213";
   Serial.print("[HTTP] Requesting ");
   Serial.println(full_uri);
-  if (http.begin(client, full_uri)){
+  if (http.begin(full_uri)){
     int httpCode = http.GET();
 
     if (httpCode > 0){
@@ -74,16 +76,18 @@ void updateWeather(){
         const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
         DynamicJsonBuffer jsonBuffer(bufferSize);
         JsonObject& root = jsonBuffer.parseObject(payload);
-        JsonObject& weather = root["weather"];
+        JsonObject& weather = root["weather"][0];
         JsonObject& temps = root["main"];
 
-        forecast = weather["main"];
+        const char* c_forecast = weather["main"];
+        forecast = (char*) c_forecast;
         temp_current = (int) temps["temp"];
         temp_high = (int) temps["temp_max"];
         temp_low = (int) temps["temp_min"];
         
         Serial.print("Forecast: ");
         Serial.println(forecast);
+        Serial.println(forecast == "Clear");
         Serial.print("Current Temp: ");
         Serial.println(temp_current);
         Serial.print("High Temp: ");
@@ -123,9 +127,9 @@ void loop(){
 
   client.loop();
   if (millis() > timer_pointer){
-    timer_pointer = millis() + five_minutes;
+    timer_pointer = millis() + 20000;
 
-    updateWeather()
+    updateWeather();
   }
 
   if ( temp_low < jacket_threshold ){
@@ -134,15 +138,13 @@ void loop(){
     digitalWrite(JACKET_LIGHT, false);
   }
 
-  if ( forecast == "Rain" ){
+  if ( strcmp(forecast, "Rain") == 0 ){
     digitalWrite(UMBRELLA_LIGHT, true);
-    digitalWrite(BOOTS_LIGHT, true);
   } else {
     digitalWrite(UMBRELLA_LIGHT, false);
-    digitalWrite(BOOTS_LIGHT, false);
   }
 
-  if ( forecast == "Snow" ) {
+  if ( strcmp(forecast, "Snow") == 0 || strcmp(forecast, "Rain") == 0 ) {
     digitalWrite(BOOTS_LIGHT, true);
   } else {
     digitalWrite(BOOTS_LIGHT, false);
