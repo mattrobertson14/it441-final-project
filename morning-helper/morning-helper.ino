96c1d36ffaa0d99f84f94e11ab9e90e1dd86481a
Matthew Robertson <mattrobertson1414@gmail.com>
2019-12-12 07:23:26 -0700

    finished project!

--- morning-helper/morning-helper.ino
+++ morning-helper/morning-helper.ino
@@ -1,15 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-#include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "./config.h"
 
 ESP8266WiFiMulti wifiMulti;
 WiFiClient wifiClient;
 HTTPClient http;
-PubSubClient client("172.20.10.14", 1883, wifiClient);
-String device_name = "esp8266-stop-light";
 const char* ssid1 = MATT_HOTSPOT_SSID;
 const char* passwd1 = MATT_HOTSPOT_PASSWORD;
 
@@ -25,6 +26,52 @@ int temp_current = 0;
 int temp_high = 0;
 int temp_low = 0;
 char* forecast = "";
+char* currentUser = "Matt";
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+static const unsigned char PROGMEM wifi_icon[] =
+{
+  0b00000000, 0b00000000, //                 
+  0b00000111, 0b11100000, //      ######     
+  0b00011111, 0b11111000, //    ##########   
+  0b00111111, 0b11111100, //   ############  
+  0b01110000, 0b00001110, //  ###        ### 
+  0b01100111, 0b11100110, //  ##  ######  ## 
+  0b00001111, 0b11110000, //     ########    
+  0b00011000, 0b00011000, //    ##      ##   
+  0b00000011, 0b11000000, //       ####      
+  0b00000111, 0b11100000, //      ######     
+  0b00000100, 0b00100000, //      #    #     
+  0b00000001, 0b10000000, //        ##       
+  0b00000001, 0b10000000, //        ##       
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000000 //                 
+};
+
+static const unsigned char PROGMEM check_icon[] =
+{
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000111, //              ###
+  0b00000000, 0b00001111, //             ####
+  0b00000000, 0b00011111, //            #####
+  0b01110000, 0b00111110, //  ###      ##### 
+  0b01111000, 0b01111100, //  ####    #####  
+  0b01111100, 0b11111000, //  #####  #####   
+  0b00011111, 0b11110000, //    #########    
+  0b00001111, 0b11100000, //     #######     
+  0b00000111, 0b11000000, //      #####      
+  0b00000011, 0b10000000, //       ###       
+  0b00000000, 0b00000000, //                 
+  0b00000000, 0b00000000 //                 
+};
 
 void connectToWifi(){
   WiFi.mode(WIFI_STA);
@@ -48,21 +95,12 @@ void connectToWifi(){
   }
 }
 
-void reconnectToHub() {
-  while (!client.connected()){
-    if (client.connect((char*) device_name.c_str(), "mqtt", "awesome")){
-      Serial.println("Connected to MQTT Hub");
-      String message = "Stoplight (" + device_name + ") connected @ " + WiFi.localIP().toString();
-      client.publish("/connections/stop-light", message.c_str());
-      client.publish("homeassistant/sensor/jacket-threshold/config", "{\"name\": \"jacket-threshold\", \"state_topic\": \"/morning-helper/jacket-threshold\"}"); 
-    } else {
-      Serial.println("Connection to MQTT failed, trying again...");
-      delay(3000);
-    }  
-  }  
-}
-
 void updateWeather(){
+  char* old_forecast = forecast;
+  int old_temp_current = temp_current;
+  int old_temp_high = temp_high;
+  int old_temp_low = temp_low;
+  
   String full_uri = "http://api.openweathermap.org/data/2.5/weather?zip=84604,us&units=Imperial&APPID=65afbab12772f80f78a291fe057de213";
   Serial.print("[HTTP] Requesting ");
   Serial.println(full_uri);
@@ -85,15 +123,6 @@ void updateWeather(){
         temp_high = (int) temps["temp_max"];
         temp_low = (int) temps["temp_min"];
         
-        Serial.print("Forecast: ");
-        Serial.println(forecast);
-        Serial.println(forecast == "Clear");
-        Serial.print("Current Temp: ");
-        Serial.println(temp_current);
-        Serial.print("High Temp: ");
-        Serial.println(temp_high);
-        Serial.print("Low Temp: ");
-        Serial.println(temp_low);
       } else {
         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
@@ -104,28 +133,82 @@ void updateWeather(){
     }
   }
 
-  updateDisplay();
+  if (strcmp(forecast, old_forecast) != 0 || old_temp_current != temp_current || old_temp_high != temp_high || old_temp_low != temp_low){
+    updateDisplay(); 
+  }  
 }
 
 void updateDisplay(){
-  Serial.println("Updating Screen...");
+  Serial.println("[OLED] Updating Screen...");
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, " + String(currentUser));
+  display.println();
+  display.println("Forecast: " + String(forecast));
+  display.println();
+  display.print("Temp: " + String(temp_current));
+  display.print((char)247);
+  display.println("F");
+  display.println();  
+  display.print("High: " + String(temp_high));
+  display.print((char)247);
+  display.println("F");
+  display.print("Low:  " + String(temp_low));
+  display.print((char)247);
+  display.println("F");
+
+  display.display();
 }
 
 void setup(){
   Serial.begin(115200);
-  connectToWifi();
+  
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("[OLED]\tSSD1306 allocation failed");
+    for(;;);
+  }
 
   pinMode(JACKET_LIGHT, OUTPUT);
   pinMode(BOOTS_LIGHT, OUTPUT);
   pinMode(UMBRELLA_LIGHT, OUTPUT);
+  
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  // Display static text
+  display.println("Connecting to WiFi...");
+  display.drawBitmap(56, 24, wifi_icon, 16, 16, 1);
+  display.display();
+  
+  connectToWifi();
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  // Display static text
+  display.println("Connected!");
+  display.drawBitmap(56, 24, check_icon, 16, 16, 1);
+  display.display();
+
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  // Display static text
+  display.println("Starting Services...");
+  display.display();
 }
 
 void loop(){
-  if (!client.connected()){
-    reconnectToHub();
-  }
-
-  client.loop();
   if (millis() > timer_pointer){
     timer_pointer = millis() + 20000;
 
